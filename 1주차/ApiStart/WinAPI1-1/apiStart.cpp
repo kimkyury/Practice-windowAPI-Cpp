99703d960c1f6ec2190c715dfc812015ccde70a2
kimkyury <mandarining0918@gmail.com>
2021-10-10 09:57:24 +0900

    Feat: 마우스이벤트, 구조체를 통한 도형 출력

--- 1주차/ApiStart/WinAPI1-1/apiStart.cpp
+++ 1주차/ApiStart/WinAPI1-1/apiStart.cpp
@@ -38,12 +38,27 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance
 }
 
 
-int 
+/*
+int p[2][200];// 도형의 좌표 저장
+int iCount; // 사각형의 갯수 저장
+*/
+
+/* 구조체 생성과 선언 */
+typedef struct tagP {
+	int x;
+	int y;
+} pxy;
+
+pxy p[1000]; // 구조체 배열 선언
+int iCount;
+int x, y; // 현재 좌표
+HBRUSH hBrush; 
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
 {
 	HDC hdc;
 	PAINTSTRUCT ps;
+
 
 	switch (iMessage) {
 	case WM_CREATE:
@@ -52,10 +67,24 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
 	case WM_LBUTTONDOWN:
 		/* 마우스 좌클릭 이벤트 작성 */
 		hdc = GetDC(hWnd);
-		Rectangle(hdc, 10, 10, 60, 60); // (10,10) 위치에 60x60사이즈 출력
+		x = LOWORD(lParam); //매크로(Mecro), 캐스팅을 통해서도 가능(x = (WORD)(lParam) )
+		y = HIWORD(lParam); 
+		/* p[0][iCount] = x;
+		p[1][iCount] = y; */
+		p[iCount].x = x;
+		p[iCount].y = y;
+		iCount++;
+		hBrush = CreateSolidBrush(RGB(0, 0, 255));
+		SelectObject(hdc, hBrush);
+		Rectangle(hdc, x - 10, y - 10, x + 60, y + 60); // 마우스로부터 (10,10) 떨어진 위치에 60x60 사이즈 출력
 		return 0;
 	case WM_PAINT:
 		hdc = BeginPaint(hWnd, &ps);
+		for (int i = 0; i < iCount; i++) {
+			hBrush = CreateSolidBrush(RGB(0, 0, 255));
+			SelectObject(hdc, hBrush);
+			Rectangle(hdc, p[i].x - 10, p[i].y - 10, p[i].x + 60, p[i].y + 60);
+		}
 		EndPaint(hWnd, &ps);
 		return 0;
 	case WM_DESTROY:
