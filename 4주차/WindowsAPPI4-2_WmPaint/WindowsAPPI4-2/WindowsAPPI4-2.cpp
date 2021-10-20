843b635a15d67f8e9343fc4aa0d90967098874c5
kimkyury <mandarining0918@gmail.com>
2021-10-20 22:32:46 +0900

    Feat: 무효화 영역 및 깜빡임 테스트

---
+++ 4주차/WindowsAPPI4-2_WmPaint/WindowsAPPI4-2/WindowsAPPI4-2.cpp
@@ -0,0 +1,82 @@
+﻿#include <windows.h>
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+HINSTANCE g_hInst;
+HWND hWndMain;
+LPCTSTR lpszClass = TEXT("WmPaint");
+
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance
+	, LPSTR lpszCmdParam, int nCmdShow)
+{
+	HWND hWnd;
+	MSG Message;
+	WNDCLASS WndClass;
+	g_hInst = hInstance;
+
+	WndClass.cbClsExtra = 0;
+	WndClass.cbWndExtra = 0;
+	WndClass.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	WndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	WndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	WndClass.hInstance = hInstance;
+	WndClass.lpfnWndProc = WndProc;
+	WndClass.lpszClassName = lpszClass;
+	WndClass.lpszMenuName = NULL;
+	WndClass.style = CS_HREDRAW | CS_VREDRAW;
+	RegisterClass(&WndClass);
+
+	hWnd = CreateWindow(lpszClass, lpszClass, WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+		NULL, (HMENU)NULL, hInstance, NULL);
+	ShowWindow(hWnd, nCmdShow);
+
+	while (GetMessage(&Message, NULL, 0, 0)) {
+		TranslateMessage(&Message);
+		DispatchMessage(&Message);
+	}
+	return (int)Message.wParam;
+}
+
+BOOL bRect = TRUE;
+LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
+{
+	PAINTSTRUCT ps;
+	HDC hdc;
+	RECT rt;
+	int i;
+	switch (iMessage) {
+	case WM_PAINT:
+		hdc = BeginPaint(hWnd, &ps);
+		for (i = 0; i < 1024; i += 2) {
+			MoveToEx(hdc, 0, i, NULL);
+			LineTo(hdc, 1280, i);
+		}
+		for (i = 0; i < 1280; i += 2) {
+			MoveToEx(hdc, i, 0, NULL);
+			LineTo(hdc, i, 1024);
+		}
+		if (bRect) {
+			Rectangle(hdc, 10, 10, 200, 200);
+		}
+		else {
+			Ellipse(hdc, 10, 10, 200, 200);
+		}
+		EndPaint(hWnd, &ps);
+		return 0;
+	case WM_LBUTTONDOWN:
+		bRect = !bRect;
+		//SetRect(&rt,10,10,200,200);
+		SetRect(&rt, 10, 10, 100, 100);
+
+		InvalidateRect(hWnd, &rt, TRUE);
+		//InvalidateRect(hWnd,NULL,TRUE); // 심한 깜빡임 발생
+		//SendMessage(hWnd, WM_PAINT, 0,0);	// 이렇게 하지 말자.
+		//UpdateWindow(hWnd);
+		//Sleep(1000);
+		return 0;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	}
+	return(DefWindowProc(hWnd, iMessage, wParam, lParam));
+}
