b2fd3a0e5a35fda480aaa1f5b6018a52c91f478c
kimkyury <mandarining0918@gmail.com>
2021-10-21 16:27:39 +0900

    Feat: 선그리기(초기화문제존재)

---
+++ 5주차/WindowsAPI5-1_MouseMKBUTTON/WindowsAPI5-1/소스.cpp
@@ -0,0 +1,74 @@
+#include <windows.h>
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+HINSTANCE g_hInst;
+LPCTSTR lpszClass = TEXT("Mouse");
+
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance
+	, LPSTR lpszCmdParam, int nCmdShow)
+{
+	HWND hWnd;
+	MSG Message;
+	WNDCLASS WndClass;
+	g_hInst = hInstance;
+
+	WndClass.cbClsExtra = 0;
+	WndClass.cbWndExtra = 0;
+	WndClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	WndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	WndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	WndClass.hInstance = hInstance;
+	WndClass.lpfnWndProc = WndProc;
+	WndClass.lpszClassName = lpszClass;
+	WndClass.lpszMenuName = NULL;
+	WndClass.style = CS_HREDRAW | CS_VREDRAW;// | CS_DBLCLKS;
+	RegisterClass(&WndClass);
+
+	hWnd = CreateWindow(lpszClass, lpszClass, WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+		NULL, (HMENU)NULL, hInstance, NULL);
+	ShowWindow(hWnd, nCmdShow);
+
+	while (GetMessage(&Message, NULL, 0, 0)) {
+		TranslateMessage(&Message);
+		DispatchMessage(&Message);
+	}
+	return (int)Message.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
+{
+	HDC hdc;
+	static int x;
+	static int y;
+	//static BOOL bNowDraw=FALSE;
+
+	switch (iMessage) {
+	case WM_LBUTTONDOWN:
+		/* 선그리기 시작을 알림 */
+		x = LOWORD(lParam);
+		y = HIWORD(lParam);
+		//	bNowDraw=TRUE;
+		return 0;
+	case WM_MOUSEMOVE:
+		if (wParam & MK_LBUTTON) {  // & = 비트연산자  // C언어에서 0 = False, 0이외의 수 = True
+			hdc = GetDC(hWnd); 
+			MoveToEx(hdc, x, y, NULL);
+			x = LOWORD(lParam);
+			y = HIWORD(lParam);
+			LineTo(hdc, x, y);
+			ReleaseDC(hWnd, hdc);
+		}
+		return 0;
+	case WM_LBUTTONUP:
+		//	bNowDraw=FALSE;
+		return 0;
+	case WM_LBUTTONDBLCLK:
+		InvalidateRect(hWnd, NULL, TRUE);
+		return 0;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	}
+	return(DefWindowProc(hWnd, iMessage, wParam, lParam));
+}
