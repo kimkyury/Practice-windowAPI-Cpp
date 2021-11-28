5cd8c63d16abe1aceec4b40f375e7114c660a75f
kimkyury <mandarining0918@gmail.com>
2021-11-28 17:37:05 +0900

    Feat: 윈도우경계 넘기

---
+++ 11주차/WindowsAPI11-3_SendMes/WindowsAPI11-3/소스.cpp
@@ -0,0 +1,67 @@
+#include <windows.h>
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+HINSTANCE g_hInst;
+HWND hWndMain;
+LPCTSTR lpszClass = TEXT("SendMes");
+
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdParam, int nCmdShow){
+	HWND hWnd;
+	MSG Message;
+	WNDCLASS WndClass;
+	g_hInst = hInstance;
+
+	WndClass.cbClsExtra = 0;
+	WndClass.cbWndExtra = 0;
+	WndClass.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	WndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	WndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	WndClass.hInstance = hInstance;
+	WndClass.lpfnWndProc = WndProc;
+	WndClass.lpszClassName = lpszClass;
+	WndClass.lpszMenuName = NULL;
+	WndClass.style = CS_HREDRAW | CS_VREDRAW;
+	RegisterClass(&WndClass);
+
+	hWnd = CreateWindow(lpszClass, lpszClass, WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+		NULL, (HMENU)NULL, hInstance, NULL);
+	ShowWindow(hWnd, nCmdShow);
+
+	while (GetMessage(&Message, NULL, 0, 0)) {
+		TranslateMessage(&Message);
+		DispatchMessage(&Message);
+	}
+	return (int)Message.wParam;
+}
+
+#define WM_SORI WM_USER+1
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam) {
+	HDC hdc;
+	PAINTSTRUCT ps;
+	HWND hWndSori;
+	TCHAR *Mes = TEXT("상대 프로그램에 메시지 발송");
+
+	switch (iMessage) {
+	case WM_LBUTTONDOWN:
+		hWndSori = FindWindow(NULL, TEXT("UserMes"));
+		if (hWndSori == NULL) {
+			MessageBox(hWnd, TEXT(" 상대프로그램이 존재하지 않습니다."),
+				TEXT("이런!"), MB_OK);
+		}
+		else {
+			SendMessage(hWndSori, WM_SORI, 0, 0);
+		}
+		return 0;
+	case WM_PAINT:
+		hdc = BeginPaint(hWnd, &ps);
+		TextOut(hdc, 50, 50, Mes, lstrlen(Mes));
+		EndPaint(hWnd, &ps);
+		return 0;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	}
+	return (DefWindowProc(hWnd, iMessage, wParam, lParam));
+}
